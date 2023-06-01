5991e1d52b5e3a2e0d6059919c81638f62335a47
Kristina Loboda <kris26tina06@gmail.com>
2023-06-01 14:09:48 +0200

    Section 6 - Addr'Product' CRUD operations.

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -7,30 +7,37 @@ namespace BulkyBook.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
         public string Title { get; set; }
+
         [Required]
         public string ISBN { get; set; }
+
         [Required]
-        public decimal Author { get; set; }
+        public string Author { get; set; }
+
+        [Required] 
+        public string Description { get; set; }
+
         [Required]
         [DisplayName("List Price")]
         [Range(1, 1000)]
-        public string ListPrice { get; set; }
+        public double ListPrice { get; set; }
 
         [Required]
         [DisplayName("Price for 1-50")]
         [Range(1, 1000)]
-        public string Price { get; set; }
+        public double Price { get; set; }
 
         [Required]
         [DisplayName("Price for 50+")]
         [Range(1, 1000)]
-        public string Price50 { get; set; }
+        public double Price50 { get; set; }
 
         [Required]
         [DisplayName("Price for 100+")]
         [Range(1, 1000)]
-        public string Price100 { get; set; }
+        public double Price100 { get; set; }
     }
 }
