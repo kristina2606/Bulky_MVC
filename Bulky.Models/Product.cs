604fe26564270804fa69628d6738ff0901ed7a81
Kristina Loboda <kris26tina06@gmail.com>
2023-06-01 11:20:22 +0200

    Section 6 - Create 'Product' model.

---
+++ Bulky.Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public decimal Author { get; set; }
+        [Required]
+        [DisplayName("List Price")]
+        [Range(1, 1000)]
+        public string ListPrice { get; set; }
+
+        [Required]
+        [DisplayName("Price for 1-50")]
+        [Range(1, 1000)]
+        public string Price { get; set; }
+
+        [Required]
+        [DisplayName("Price for 50+")]
+        [Range(1, 1000)]
+        public string Price50 { get; set; }
+
+        [Required]
+        [DisplayName("Price for 100+")]
+        [Range(1, 1000)]
+        public string Price100 { get; set; }
+    }
+}
