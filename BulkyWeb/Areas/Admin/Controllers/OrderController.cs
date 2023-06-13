fc2305a1d5138ebb8b870874502a32e0e7903ff7
Kristina Loboda <kris26tina06@gmail.com>
2023-06-13 15:40:24 +0200

    Section 12 - Loading Order Details Header.

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using BulkyBook.Utility;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -21,14 +22,25 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult Details(int orderId)
+        {
+            OrderVM orderVM = new()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+                OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
+            };
+
+            return View(orderVM);
+        }
+
         #region API Calls
 
         [HttpGet]
         public IActionResult GetAll(string status)
         {
             IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
-           
-            switch(status)
+
+            switch (status)
             {
                 case "pending":
                     objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
@@ -45,7 +57,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                 default:
                     break;
             }
-            
+
             return Json(new { data = objOrderHeaders });
         }
 
