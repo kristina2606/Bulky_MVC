c011f7f047da64af2758d7494bb76da98d659a81
Kristina Loboda <kris26tina06@gmail.com>
2023-06-18 14:36:43 +0200

    Section 16 - Display Image on Update Product Page.

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -45,7 +45,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             }
             else
             {
-                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id, includeProperties:"ProductImages");
                 return View(productVM);
             }
         }
