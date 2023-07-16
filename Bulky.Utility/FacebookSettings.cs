546ba8438546471584657a3e5843146f375a1804
Kristina Loboda <kris26tina06@gmail.com>
2023-07-16 15:47:53 +0200

    Change appsetting.json

---
+++ Bulky.Utility/FacebookSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace BulkyBook.Utility
+{
+    public class FacebookSettings
+    {
+        public string AppId { get; set; }
+        public string AppSecret { get; set; }
+    }
+}
---
+++ Bulky.Utility/MicrosoftAccountSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace BulkyBook.Utility
+{
+    public class MicrosoftAccountSettings
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
